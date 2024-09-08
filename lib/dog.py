36bb36d2aaf88f558f0cdfa848f65dee1dd7b274
kacha930 <robiwere20@gmail.com>
2024-09-08 11:10:25 +0300

    proper understanding of how to implement instance methods to a class

--- lib/dog.py
+++ lib/dog.py
@@ -2,6 +2,21 @@
 
 class Dog:
     # Class body goes here
-
+    
     #Instance method definition
-    pass
+     def bark(self):
+         print("Woof!")
+         
+     def sit(self):
+         print("The dog is sitting.")
+
+    
+    #  Now let's create an instance of Dog and verify that our Dog object knows how to bark:
+
+fido = Dog()
+fido.bark()
+fido.sit()      
+        
+
+
+        
